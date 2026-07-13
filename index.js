33ab0ae10d60eb991b6190a359eaf6d4d07354ca
kelobapan-cpu <kelobapan@gmail.com>
2026-07-13 09:33:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+import express from "express";
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+
+    res.send("✅ NexVora BharatPe Backend Running");
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+    console.log("Server Running On Port " + PORT);
+
+});
